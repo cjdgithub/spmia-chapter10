4fd1c4824dc2969149a98948297861973e56ddf1
cjdgithub <foo@bar.com>
2018-05-23 23:20:12 +0100

    Updating travis scripts

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,6 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region eu-west-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
-ecs-cli compose --file docker/common/docker-compose.yml up
+ecs-cli configure profile --profile-name spmia-tmx-dev --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli up --instance-role ecsInstanceRole --instance-type t2.large --cluster spmia-tmx-dev --region eu-west-2 --keypair id_rsa --force --project-name spmia-tmx-dev
+ecs-cli compose --verbose --file docker/common/docker-compose.yml --project-name spmia-tmx-dev --cluster spmia-tmx-dev --region eu-west-2 up
+ecs-cli ps
 rm -rf ~/.ecs
