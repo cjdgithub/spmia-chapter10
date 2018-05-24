fabf22c423e424f0be92ea01ef78f7bafeb62f47
cjdgithub <foo@bar.com>
2018-05-24 13:49:11 +0100

    Updating images to use cjddockerhub

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push cjdgithub/tmx-authentication-service:$BUILD_NAME
-docker push cjdgithub/tmx-licensing-service:$BUILD_NAME
-docker push cjdgithub/tmx-organization-service:$BUILD_NAME
-docker push cjdgithub/tmx-confsvr:$BUILD_NAME
-docker push cjdgithub/tmx-eurekasvr:$BUILD_NAME
-docker push cjdgithub/tmx-zuulsvr:$BUILD_NAME
+docker push cjddockerhub/tmx-authentication-service:$BUILD_NAME
+docker push cjddockerhub/tmx-licensing-service:$BUILD_NAME
+docker push cjddockerhub/tmx-organization-service:$BUILD_NAME
+docker push cjddockerhub/tmx-confsvr:$BUILD_NAME
+docker push cjddockerhub/tmx-eurekasvr:$BUILD_NAME
+docker push cjddockerhub/tmx-zuulsvr:$BUILD_NAME
